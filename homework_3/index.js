5d485691ad1f214b2d1a3eac474f46a35cb04464
OlgaSuslova <alekssportmen@gmail.com>
2024-03-27 14:12:00 +0300

    added 3 homework

---
+++ homework_3/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('static'));
+
+const countPage = path.join(__dirname, 'count.json');
+const counter = JSON.parse(fs.readFileSync(countPage, 'utf-8'));
+
+
+app.get('/', (req, res) => {
+    counter.index++;
+    fs.writeFileSync(countPage, JSON.stringify(counter, null, 2));
+    res.send(`<h1>Корневая страница</h1><p>Просмотров: ${counter.index}</p><a href="./about">About</a>`)
+})
+
+app.get('/about', (req, res) => {
+    counter.about++;    
+    fs.writeFileSync(countPage, JSON.stringify(counter, null, 2));
+    res.send(`<h1>Страница About</h1><p>Просмотров: ${counter.about}</p><a href="./">Index</a>`);
+})
+
+app.listen(3000);
+
